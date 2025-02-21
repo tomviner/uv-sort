f6078c24acbab673a4164d8e0452732b72655147
Manabu Niseki <manabu.niseki@gmail.com>
2025-02-22 08:44:01 +0900

    refactor: use trusted publisher & use built version

--- src/uv_sort/__init__.py
+++ src/uv_sort/__init__.py
@@ -1,3 +1,6 @@
-import importlib.metadata
+try:
+    from ._version import version
 
-__version__ = importlib.metadata.version(__name__)
+    __version__ = version
+except ImportError:
+    __version__ = "0.0.0"
